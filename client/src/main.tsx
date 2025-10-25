ba90775af468c9dc160cf695500803cd1a453d52
OGThorhunter <tttrcasey@gmail.com>
2025-10-25 13:32:40 -0400

    forcepush3

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { ErrorBoundary } from 'react-error-boundary';
 import { QueryProvider } from './lib/query';
 import { HelmetProvider } from 'react-helmet-async';
+import { PostHogProvider } from 'posthog-js/react';
 import './index.css';
 import App from './App.tsx';
 import { setupGlobalErrorHandlers, ErrorFallback } from './utils/errorHandlers.tsx';
@@ -19,14 +20,22 @@ setupGlobalErrorHandlers();
 // Mute extension noise in development
 muteExtensionNoise();
 
-
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <QueryProvider>
         <HelmetProvider>
-          <App />
+          <PostHogProvider
+            apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+            options={{
+              api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+              defaults: '2025-05-24',
+              capture_exceptions: true,
+              debug: import.meta.env.MODE === 'development',
+            }}
+          >
+            <App />
+          </PostHogProvider>
         </HelmetProvider>
       </QueryProvider>
     </ErrorBoundary>
